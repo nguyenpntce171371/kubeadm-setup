f6921b5b5cdd1bafd77a48456d9ba2eacd6d332c
nguyenpntce171371 <161195542+nguyenpntce171371@users.noreply.github.com>
2025-03-22 15:13:56 +0700

    Update kubeadm-setup-master.sh

--- kubeadm-setup-master.sh
+++ kubeadm-setup-master.sh
@@ -35,8 +35,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-apt-get install -y kubelet kubeadm kubectl containerd
-apt-get install -y docker.io
+apt-get install -y kubelet kubeadm kubectl containerd docker.io
 apt-mark hold kubelet kubeadm kubectl docker.io
 mkdir /etc/containerd
 containerd config default > /etc/containerd/config.toml
@@ -53,7 +52,7 @@ systemctl enable docker.service
 #----------------------
 
 kubeadm config images pull
-kubeadm init --pod-network-cidr=192.168.0.0/16 --kubernetes-version=1.32.3 --ignore-preflight-errors=all
+kubeadm init --pod-network-cidr=192.168.56.10/16 --pod-network-cidr=192.168.0.0/16 --kubernetes-version=1.32.3 --ignore-preflight-errors=all
 
 #------------------
 
