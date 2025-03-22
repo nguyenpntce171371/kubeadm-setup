78d746d863dd92af551a1c9028013154c18994d4
nguyenpntce171371 <161195542+nguyenpntce171371@users.noreply.github.com>
2025-03-22 12:57:15 +0700

    Update kubeadm-setup-master.sh

--- kubeadm-setup-master.sh
+++ kubeadm-setup-master.sh
@@ -23,26 +23,20 @@ cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-iptables = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward = 1
-
 EOF
 
 #--------------------
 
 sysctl --system
 
-#-------------
-
-mkdir /etc/apt/keyrings
-curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-
 #-----------
 
-apt update
-apt install -y apt-transport-https ca-certificates curl
-apt install -y kubelet kubeadm docker.io
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+sudo apt-get install -y apt-transport-https ca-certificates curl
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.32/deb/Release.key | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.32/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+apt-get install -y kubelet kubeadm kubectl containerd
+apt-get install -y docker.io
 apt-mark hold kubelet kubeadm kubectl docker.io
 mkdir /etc/containerd
 containerd config default > /etc/containerd/config.toml
